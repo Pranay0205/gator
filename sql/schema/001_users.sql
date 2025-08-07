44cdf2d9367a394222524a818b947811c4e7d684
Pranay0205 <pranayghuge@gmail.com>
2025-08-06 23:06:31 -0400

    feat: create posts table

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE users(
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL,
-  name VARCHAR(100) UNIQUE NOT NULL
+  name TEXT UNIQUE NOT NULL
 );
 -- +goose Down
 DROP TABLE users;
--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -3,8 +3,8 @@ CREATE TABLE feeds(
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL,
-  name VARCHAR(100) UNIQUE NOT NULL,
-  url VARCHAR(255) UNIQUE NOT NULL,
+  name TEXT UNIQUE NOT NULL,
+  url TEXT UNIQUE NOT NULL,
   user_id UUID NOT NULL,
   CONSTRAINT fk_user
     FOREIGN KEY(user_id) 
